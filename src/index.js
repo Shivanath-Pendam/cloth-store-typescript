6ce8d05fa809d57329cbe8b64b01b222f33935b2
Shivanath <125187731+Shivanath-Pendam@users.noreply.github.com>
2024-03-10 19:46:40 +0530

    add payment to application

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
